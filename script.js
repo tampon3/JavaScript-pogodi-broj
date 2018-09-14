8baf27a0059ad512a82c6207e41fa7d00ea4c5ca
tampon3 <43275026+tampon3@users.noreply.github.com>
2018-09-14 17:20:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// Uzmi nasumičan - random broj od 1 do 10
+var numero = Math.ceil(Math.random() * 10);  
+
+ 
+var slanac = prompt("pgodi broj izmedu jedan i deset");
+
+if (slanac == numero) {
+	
+	alert ("ti si genije, cudo prirode");
+	
+}
+else
+{
+	
+	alert ("nebu išlo, traženi broj je:" + numero);
+	
+}
+ 
+ 
+ 
+ 
+ 
+ 
